6deb6657023cc7507f91a74ec4b6812afc6e972f
Cristian martini <cristianmartini.sp@gmail.com>
2024-10-20 19:12:13 -0300

    modificaçoes formulario e login , tamanho de tela

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { CssBaseline } from '@mui/material';
 import { ThemeProvider } from '@mui/material/styles';
 import AppRoutes from './routes';
 import theme from './theme';
+import './index.css'; // Importando os estilos globai
 
 function App() {
   return (
