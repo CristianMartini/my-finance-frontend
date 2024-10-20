40c70fac7ad4982daa5923242a935171865d11ab
Cristian martini <cristianmartini.sp@gmail.com>
2024-10-20 20:41:57 -0300

    tabela melhorada com limite de caracteres,e posiçao dos elementos

--- frontend/src/components/TransactionTable/index.tsx
+++ frontend/src/components/TransactionTable/index.tsx
@@ -1,5 +1,4 @@
-//frontend\src\components\TransactionTable\index.tsx
-import React from 'react';
+import React, { useState } from 'react';
 import {
   Table,
   TableBody,
@@ -12,8 +11,14 @@ import {
   Typography,
   useMediaQuery,
   Box,
+  Dialog,
+  DialogTitle,
+  DialogContent,
+  DialogActions,
+  Button,
+  Tooltip,
 } from '@mui/material';
-import { Edit, Delete } from '@mui/icons-material';
+import { Edit, Delete, StickyNote2 } from '@mui/icons-material'; // Alterando o ícone de nota para StickyNote2
 import { useTheme } from '@mui/material/styles';
 import { Transaction } from '../../types';
 
@@ -31,6 +36,27 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
 
+  // Estado para abrir o modal com a nota
+  const [noteOpen, setNoteOpen] = useState(false);
+  const [selectedNote, setSelectedNote] = useState<string | null>(null);
+
+  const handleNoteOpen = (note: string | undefined) => {
+    setSelectedNote(note ?? null); // Corrige para garantir que 'undefined' seja tratado como 'null'
+    setNoteOpen(true);
+  };
+
+  const handleNoteClose = () => {
+    setNoteOpen(false);
+    setSelectedNote(null);
+  };
+
+  const truncateText = (text: string, limit: number) => {
+    if (text.length > limit) {
+      return text.substring(0, limit) + '...';
+    }
+    return text;
+  };
+
   if (transactions.length === 0) {
     return (
       <Typography variant="h6" align="center" mt={4}>
@@ -45,12 +71,12 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
         <Table>
           <TableHead>
             <TableRow>
-              <TableCell>Descrição</TableCell>
-              <TableCell>Valor</TableCell>
-              <TableCell>Data</TableCell>
-              <TableCell>Categoria</TableCell>
-              <TableCell>Tipo</TableCell>
-              <TableCell>Fonte</TableCell>
+              <TableCell sx={{ maxWidth: '200px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>Descrição</TableCell>
+              <TableCell sx={{ textAlign: 'center' }}>Valor</TableCell>
+              <TableCell sx={{ textAlign: 'center' }}>Data</TableCell>
+              <TableCell sx={{ textAlign: 'center' }}>Categoria</TableCell>
+              <TableCell sx={{ textAlign: 'center' }}>Tipo</TableCell>
+              <TableCell sx={{ textAlign: 'center' }}>Fonte</TableCell>
               <TableCell align="center">Ações</TableCell>
             </TableRow>
           </TableHead>
@@ -65,32 +91,43 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
                   },
                 }}
               >
-                <TableCell>{transaction.description}</TableCell>
-                <TableCell>
+                <TableCell sx={{ maxWidth: '200px', whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}>
+                  <Tooltip title={transaction.description} placement="top">
+                    <Typography>
+                      {truncateText(transaction.description, 25)}
+                    </Typography>
+                  </Tooltip>
+                </TableCell>
+                <TableCell sx={{ textAlign: 'center' }}>
                   <Typography
                     color={transaction.category === 'income' ? 'success.main' : 'error.main'}
                   >
                     {transaction.category === 'income' ? '+' : '-'} R${' '}
-                    {transaction.amount.toFixed(2)}
+                    {transaction.amount.toFixed(2).slice(0, 10)}
                   </Typography>
                 </TableCell>
-                <TableCell>{new Date(transaction.date).toLocaleDateString()}</TableCell>
-                <TableCell>{transaction.subCategory}</TableCell>
-                <TableCell>{transaction.type}</TableCell>
-                <TableCell>{transaction.source}</TableCell>
-                <TableCell align="center">
-                  <IconButton
-                    color="primary"
-                    onClick={() => onEdit(transaction)}
-                  >
+                <TableCell sx={{ textAlign: 'center' }}>
+                  {new Date(transaction.date).toLocaleDateString()}
+                </TableCell>
+                <TableCell sx={{ textAlign: 'center' }}>{transaction.subCategory}</TableCell>
+                <TableCell sx={{ textAlign: 'center' }}>{transaction.type}</TableCell>
+                <TableCell sx={{ textAlign: 'center' }}>{transaction.source}</TableCell>
+                <TableCell sx={{ textAlign: 'center', display: 'flex', justifyContent: 'center' }}>
+                  <IconButton color="primary" onClick={() => onEdit(transaction)}>
                     <Edit />
                   </IconButton>
-                  <IconButton
-                    color="error"
-                    onClick={() => onDelete(transaction)}
-                  >
+                  <IconButton color="error" onClick={() => onDelete(transaction)}>
                     <Delete />
                   </IconButton>
+
+                  {/* Ícone de nota com espaço reservado, mesmo que não haja nota */}
+                  <Box sx={{ width: 48, textAlign: 'center' }}>
+                    {transaction.notes && (
+                      <IconButton color="info" onClick={() => handleNoteOpen(transaction.notes)}>
+                        <StickyNote2 /> {/* Ícone de nota mais adequado */}
+                      </IconButton>
+                    )}
+                  </Box>
                 </TableCell>
               </TableRow>
             ))}
@@ -99,11 +136,7 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
       ) : (
         <Box display="flex" flexDirection="column" gap={2}>
           {transactions.map((transaction) => (
-            <Paper
-              key={transaction._id}
-              elevation={2}
-              sx={{ p: 2, backgroundColor: '#f9f9f9' }}
-            >
+            <Paper key={transaction._id} elevation={2} sx={{ p: 2, backgroundColor: '#f9f9f9' }}>
               <Box>
                 <Typography variant="subtitle1" fontWeight="bold">
                   {transaction.description}
@@ -112,7 +145,7 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
                   color={transaction.category === 'income' ? 'success.main' : 'error.main'}
                 >
                   {transaction.category === 'income' ? '+' : '-'} R${' '}
-                  {transaction.amount.toFixed(2)}
+                  {transaction.amount.toFixed(2).slice(0, 10)}
                 </Typography>
                 <Typography variant="body2">
                   {new Date(transaction.date).toLocaleDateString()}
@@ -122,23 +155,37 @@ const TransactionTable: React.FC<TransactionTableProps> = ({
                 <Typography variant="body2">{transaction.source}</Typography>
               </Box>
               <Box display="flex" justifyContent="flex-end" mt={1}>
-                <IconButton
-                  color="primary"
-                  onClick={() => onEdit(transaction)}
-                >
+                <IconButton color="primary" onClick={() => onEdit(transaction)}>
                   <Edit />
                 </IconButton>
-                <IconButton
-                  color="error"
-                  onClick={() => onDelete(transaction)}
-                >
+                <IconButton color="error" onClick={() => onDelete(transaction)}>
                   <Delete />
                 </IconButton>
+
+                {/* Ícone de nota com espaço reservado, mesmo que não haja nota */}
+                <Box sx={{ width: 48, textAlign: 'center' }}>
+                  {transaction.notes && (
+                    <IconButton color="info" onClick={() => handleNoteOpen(transaction.notes)}>
+                      <StickyNote2 /> {/* Ícone de nota mais adequado */}
+                    </IconButton>
+                  )}
+                </Box>
               </Box>
             </Paper>
           ))}
         </Box>
       )}
+
+      {/* Modal para exibir as notas */}
+      <Dialog open={noteOpen} onClose={handleNoteClose}>
+        <DialogTitle>Nota da Transação</DialogTitle>
+        <DialogContent>
+          <Typography>{selectedNote}</Typography>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleNoteClose}>Fechar</Button>
+        </DialogActions>
+      </Dialog>
     </TableContainer>
   );
 };
